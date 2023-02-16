fbb4ca66813a37ccba552fe683b4bdc530506289
Camilo Gil Franco <camilogilfranco@gmail.com>
2023-02-16 14:25:42 -0500

    application of styles

--- src/components/SingleTodoTask/SingleTodoTask.jsx
+++ src/components/SingleTodoTask/SingleTodoTask.jsx
@@ -1,21 +1,31 @@
-import './SingleTodoTask.css'
-import { useState } from 'react';
-const handleOnClick = ()=> {
+import "./SingleTodoTask.css";
+import { useState } from "react";
+import check from "../../images/check-small-svgrepo-com.svg";
+import edit from "../../images/write-svgrepo-com.svg";
+import erase from "../../images/delete-1-svgrepo-com.svg";
 
-}
-const SingleTodoTask = ( { id, task }) =>{
+const handleOnClick = () => {};
+const SingleTodoTask = ({ id, task }) => {
+  const [tarea, setTarea] = useState("");
+  return (
+    <div className="singleTodo">
+      <p className="id">{id}1</p>
+      <input type="text" className="task" disabled>
+        {task}
+      </input>
+      <div className="buttons">
+        <picture onClick={handleOnClick} className="pictureCheck">
+          <img src={check} alt="check" id="check"></img>
+        </picture>
+        <picture className="pictureEdit">
+          <img src={edit} alt="edit" id="edit"></img>
+        </picture>
+        <picture className="pictureErase">
+          <img src={erase} alt="delete" id="erase"></img>
+        </picture>
+      </div>
+    </div>
+  );
+};
 
-    const [tarea, setTarea] = useState('');
-    return(
-        <div className='singleTodo'>    
-            <p>{id}</p>
-            <input type='text'>{task}</input> 
-            <picture onClick={handleOnClick}><img src='../../images/check-small-svgrepo-com.svg' alt='checkbox'></img></picture>
-            <picture><img src='../../images/check-small-svgrepo-com.svg' alt='edit'></img></picture>
-            <picture><img src='../../images/check-small-svgrepo-com.svg' alt='delete'></img></picture>
-        </div>
-    )
-}
-
-
-export default SingleTodoTask
+export default SingleTodoTask;
